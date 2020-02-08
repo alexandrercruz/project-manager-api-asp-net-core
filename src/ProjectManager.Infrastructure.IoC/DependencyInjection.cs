fd27d53e4a0a3fdc2591b67f1879b82b58bbba27
Alexandre Cruz <blackbass83@gmail.com>
2020-02-08 19:19:57 -0300

    Added ioc project to the solution.

---
+++ src/ProjectManager.Infrastructure.IoC/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProjectManager.Application.Interfaces.Core;
+using ProjectManager.Application.Services.Core;
+using ProjectManager.Domain.Interfaces;
+using ProjectManager.Domain.Interfaces.Core;
+using ProjectManager.Infrastructure.Data.Context;
+using ProjectManager.Infrastructure.Data.Repository.Core;
+using ProjectManager.Infrastructure.Data.UoW;
+
+namespace ProjectManager.Infrastructure.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            #region Application
+
+            services.AddScoped<IAccountAppService, AccountAppService>();
+
+            #endregion Application
+
+            #region Infrastructure
+
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<ProjectManagerContext>();
+
+            #endregion Infrastructure
+
+            return services;
+        }
+    }
+}
