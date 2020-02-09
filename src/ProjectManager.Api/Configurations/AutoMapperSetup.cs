8865abc5e1f78bd43648c8f3443313048a39f051
Alexandre Cruz <blackbass83@gmail.com>
2020-02-09 20:50:10 -0300

    Added api project to the solution.

---
+++ src/ProjectManager.Api/Configurations/AutoMapperSetup.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectManager.Application.AutoMapper;
+using System;
+
+namespace ProjectManager.Api.Configurations
+{
+    public static class AutoMapperSetup
+    {
+        public static void AddAutoMapperSetup(this IServiceCollection services)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+
+            services.AddAutoMapper(typeof(Startup));
+
+            // Registering Mappings automatically only works if the
+            // Automapper Profile classes are in ASP.NET project
+            AutoMapperConfig.RegisterMappings();
+        }
+    }
+}
---
+++ src/ProjectManager.Api/Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProjectManager.Api.Configurations;
+using ProjectManager.Infrastructure.Context;
+using ProjectManager.Infrastructure.IoC;
+
+namespace ProjectManager.Api
+{
+    public class Startup
+    {
+        //public Startup(IConfiguration configuration)
+        public Startup(IHostEnvironment env)
+        {
+            //Configuration = configuration;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ProjectManagerContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddAutoMapperSetup();
+
+            services.AddControllers();
+
+            services.RegisterServices();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
